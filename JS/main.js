c3fb7ab9f599e977fef6dc4098720a8195b9114e
Julius <Julius>
2020-05-01 22:35:13 +0300

    some fixes / add close open animation

--- JS/main.js
+++ JS/main.js
@@ -17,5 +17,13 @@ function tapClose() {
                 tap.className = 'links';
                 
             }
-    
+            myFunction(this);
 }
+
+// -----------------------[ MENU open / close animation ]
+
+// let close = document.querySelector('.close').onclick = myFunction;
+
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
